a4893c7d59eff41e55e64108c3832a4f3d01601b
Jamiu007 <jamiuokam72@gmail.com>
2026-09-15 12:38:58 -0700

    Add landing page and mock auth flows

--- tutortrack/src/App.jsx
+++ tutortrack/src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route, Navigate } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import { AuthProvider } from "./context/AuthContext.jsx";
 import { RoleProvider } from "./context/RoleContext.jsx";
 import { ToastProvider } from "./context/ToastContext.jsx";
@@ -8,6 +8,9 @@ import AuthPage from "./pages/auth/AuthPage.jsx";
 import TutorPage from "./pages/tutor/TutorPage.jsx";
 import StudentPage from "./pages/student/StudentPage.jsx";
 import ParentPage from "./pages/parent/ParentPage.jsx";
+import LandingPage from "./pages/landing/LandingPage.jsx";
+import SignInPage from "./pages/auth/SignInPage.jsx";
+import SignUpPage from "./pages/auth/SignUpPage.jsx";
 
 export default function App() {
   return (
@@ -16,7 +19,9 @@ export default function App() {
         <RoleProvider>
           <ToastProvider>
             <Routes>
-              <Route path="/" element={<Navigate to="/tutor" replace />} />
+              <Route path="/" element={<LandingPage />} />
+              <Route path="/signin" element={<SignInPage />} />
+              <Route path="/signup" element={<SignUpPage />} />
               <Route path="/auth" element={<AuthPage />} />
               <Route element={<DashboardLayout />}>
                 <Route path="/tutor" element={<TutorPage />} />
